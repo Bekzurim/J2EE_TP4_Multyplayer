c8be8b3ebaea90e35a13b06ef8fc8938f90dda2b
pedago <pedago@pcalb-eb0201.univ-jfc.fr>
2019-10-01 15:05:39 +0200

    Introduction des tests

---
+++ src/test/java/bowling/MultiplayerGameTest.java
@@ -0,0 +1,24 @@
+package bowling;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pedago
+ */
+public class MultiplayerGameTest {
+    
+    private MultiplayerGameTest game;
+
+	@Before
+	public void setUp() {
+		game = new MultiplayerGameTest();
+	}
+    
+    @Test
+	public void Test1() {
+            
+        }
+}
